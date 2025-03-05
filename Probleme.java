cd5bd897462501028f755cf4b5952a18c282ebd1
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-05 21:42:18 +0530

    Add files via upload
    Printing the hollow square pattern.

---
+++ Probleme.java
@@ -0,0 +1,23 @@
+/*Printing the hollow square pattern. */
+import java.util.Scanner;
+public class Probleme
+{
+	public static void main(String[] args) {
+	    Scanner obj=new Scanner(System.in);
+	    int n=obj.nextInt();
+		for(int i=0;i<n;i++){
+		    for(int j=0;j<n;j++){
+		        if(i==0||j==0||i==n-1||j==n-1){
+		            System.out.print("* ");
+		            
+		        }
+		        else{
+		            System.out.print("  ");
+		        }
+		        
+		    }
+		    System.out.print("\n");
+		    
+		}
+	}
+}
