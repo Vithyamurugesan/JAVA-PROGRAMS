9efea7d967d3fa0b5690462bd8f49cc4978cd540
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-12 21:53:21 +0530

    Add files via upload
    Sort an Array.

---
+++ Problemn.java
@@ -0,0 +1,9 @@
+//Sort an array.
+import java.util.Arrays;
+public class Problemn {
+    public static void main(String[]args){
+        int[]arr={5,2,8,1,3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }   
+}
