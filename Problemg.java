7ad75d46eb504f4e39bacb1413af09c47c50368c
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-12 21:37:13 +0530

    Add files via upload
    Reverse a string.

---
+++ Problemg.java
@@ -0,0 +1,9 @@
+//Reverse a string.
+import java.util.Scanner;
+public class Problemg{
+    public static void main(String[]args){
+        String str="Automation";
+        StringBuilder reversed=new StringBuilder(str).reverse();
+        System.out.println(reversed);
+    }
+}
