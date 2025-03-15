47cfa952a275ab424a141b6a22bdc63d4e22b4fd
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-15 22:41:27 +0530

    Add files via upload
    Reverse a number.

---
+++ Problemr.java
@@ -0,0 +1,12 @@
+//Reverse a number.
+import java.util.Scanner;
+public class Problemr{
+    public static void main(String[]args){
+        int num=1234,reversed=0;
+        while(num!=0){
+            reversed=reversed*10+num%10;
+            num/=10;    
+        }
+        System.out.println(reversed);
+    }
+}
