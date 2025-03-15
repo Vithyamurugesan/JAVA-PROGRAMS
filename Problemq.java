1e1efd321e6ecad440dfcbb8b68e628df9026152
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-15 22:32:10 +0530

    Add files via upload
    Check if a number is Armstrong.

---
+++ Problemq.java
@@ -0,0 +1,13 @@
+//Check if a number is Armstrong.
+import java.util.*;
+public class Problemq{
+    public static void main(String[]args){
+        int num=153,sum=0,temp=num;
+        while(temp!=0){
+            int digit=temp%10;
+            sum+=Math.pow(digit,3);
+            temp/=10;
+        }
+        System.out.println(num==sum);
+    }
+}
