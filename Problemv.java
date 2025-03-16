bad173a7947ba8ed706cbe86f67422b024a8cc5c
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-16 21:40:10 +0530

    Add files via upload
    Print the prime numbers in a range.

---
+++ Problemv.java
@@ -0,0 +1,19 @@
+//Print the prime numbers in a range.
+import java.util.*;
+public class Problemv{
+    public static void main(String[]args){
+        int start=10,end=50;
+        for(int num=start;num<=end;num++){
+            boolean isPrime=true;
+            for(int i=2;i<=Math.sqrt(num);i++){
+                if(num%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime && num>1){
+                System.out.println(num+"");
+            }
+        }
+    }
+}
