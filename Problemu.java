881b6476fe3307dd1fe7e9630ffb810f768a6007
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-16 21:33:04 +0530

    Add files via upload
    Count the number of digits in a number.

---
+++ Problemu.java
@@ -0,0 +1,9 @@
+//Count the number of digits in a number.
+import java.util.*;
+public class Problemu{
+    public static void main(String[]args){
+        int num=12345;
+        int count=String.valueOf(num).length();
+        System.out.println(count);
+    }
+}
