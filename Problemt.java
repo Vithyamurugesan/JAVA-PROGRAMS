23071591c5f993ac4e857c24b2e9e10f527d8d66
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-16 21:32:24 +0530

    Add files via upload
    Check for Anagram.

---
+++ Problemt.java
@@ -0,0 +1,12 @@
+//Check for Anagram.
+import java.util.*;
+public class Problemt{
+    public static void main(String[]args){
+        String str1="listen",str2="silent";
+        char[]arr1=str1.toCharArray();
+        char[]arr2=str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        System.out.println(Arrays.equals(arr1,arr2));
+    }
+}
