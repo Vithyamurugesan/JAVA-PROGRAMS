bd6247943874e29606df95ab517d52662a0b712a
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-18 22:24:16 +0530

    Add files via upload
    Find the missing number in an array.

---
+++ Problemy.java
@@ -0,0 +1,13 @@
+//Find the missing number in an array.
+import java.util.*;
+public class Problemy {
+    public static void main(String[]args){
+        int[]arr={1,2,4,5,6};
+        int n=arr.length+1;
+        int total=n*(n+1)/2;
+        for(int num:arr){
+            total-=num;
+        }
+        System.out.println(total);
+        }
+    }
