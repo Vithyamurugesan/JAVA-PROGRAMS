ca17f0fcd30d11533d0433b4c5538a582fb7dec1
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-19 21:29:56 +0530

    Add files via upload
    Check for perfect number.

---
+++ Proa.java
@@ -0,0 +1,13 @@
+//Check for Perfect number.
+public class Proa {
+    public static void main(String[]args){
+        int num=28,sum=0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        System.out.println(num==sum);
+    }
+    
+}
