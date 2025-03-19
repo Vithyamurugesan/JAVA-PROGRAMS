eb2843f4e5080f54bac217652ea4e9a32d6a7207
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-19 21:24:51 +0530

    Add files via upload
    Convert decimal to binary.

---
+++ Problemz.java
@@ -0,0 +1,10 @@
+//Convert decimal to binary.
+import java.util.*;
+public class Problemz{
+    public static void main(String[]args){
+        int num=10;
+        String binary=Integer.toBinaryString(num);
+        System.out.println(binary);
+
+    }
+}
