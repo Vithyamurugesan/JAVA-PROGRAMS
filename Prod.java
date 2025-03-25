58ba6277ad52effae1fed626ea32247dd0ab982d
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-25 22:26:51 +0530

    Add files via upload
    Sum of two integer.

---
+++ Prod.java
@@ -0,0 +1,12 @@
+//Sum of two integer.
+import java.util.*;
+public class Prod {
+    void m1(int a,int b){
+        int c=a+b;
+        System.out.println("The sum of the result is "+c);      
+    }
+    public static void main(String[]args){
+        Prod x=new Prod();
+        x.m1(4,6);      
+    }   
+}
