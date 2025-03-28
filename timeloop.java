cd11789fbe84d641f1a1431c23af952c60a2e382
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-28 22:37:46 +0530

    Add files via upload
    Time loop Kattis platform problem statement.

---
+++ timeloop.java
@@ -0,0 +1,11 @@
+//Timeloop Kattis platform problem statement.
+import java.util.Scanner;
+public class timeloop{
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        for(int i=1;i<=a;i++){
+            System.out.println(i+" "+"Abracadabra");
+        }
+    }
+}
