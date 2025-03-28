cc2067008e2029450216f1f954e531dd08c09fd4
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-28 22:32:52 +0530

    Add files via upload
    //add two numbers kattis platform problems.

---
+++ addtwonumbers.java
@@ -0,0 +1,10 @@
+//add two numbers kattis platform problems.
+import java.util.Scanner;
+public class addtwonumbers{
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        int b=input.nextInt();
+        System.out.println(a+b);
+    }
+}
