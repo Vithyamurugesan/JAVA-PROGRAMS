658616ec26d199e94fa325f29940349d2b318537
Vithyamurugesan <vithyabtech54@gmail.com>
2025-03-30 21:30:19 +0530

    Add files via upload
    Area of triangle

---
+++ triarea.java
@@ -0,0 +1,10 @@
+//Area of triangle.
+import java.util.*;
+public class triarea{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int height=sc.nextInt();
+        int base=sc.nextInt();
+        System.out.println(0.5*base*height);
+    }
+}
