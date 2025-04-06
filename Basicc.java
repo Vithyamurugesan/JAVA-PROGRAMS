4f170054c216a3cf38b9680003c52c59250bfb5a
Vithyamurugesan <vithyabtech54@gmail.com>
2025-04-06 23:36:16 +0530

    Add files via upload
    How to read a number from Standard input.

---
+++ Basicc.java
@@ -0,0 +1,13 @@
+//How to read a number from Standard input.
+import java.io.*;
+import java.util.Scanner;
+public class Basicc{
+    public static void main(String[]args){
+        int num;
+        System.out.println("Enter the integer:");
+        Scanner s=new Scanner(System.in);
+        num=s.nextInt();
+        System.out.println("Entered integer is: "+num);
+        s.close();66
+    }
+}
