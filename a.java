dc5f9183f82206e0fdbce39abed8449e25ead9de
Vithyamurugesan <vithyabtech54@gmail.com>
2025-04-11 10:16:14 +0530

    Add files via upload
    Usage of ternary operator.

---
+++ a.java
@@ -0,0 +1,12 @@
+
+//Usage of ternary operator.
+import java.util.*;
+public class a{
+    public static void main(String[]args){
+        int n=10;
+        int result=0;
+        result=n%2==0?10:32;
+        System.out.println(result);   
+    }
+}
+    
