9c91120d28a24a3f57e21e5bf5cfca4310c0a589
Vithyamurugesan <vithyabtech54@gmail.com>
2025-04-11 10:26:25 +0530

    Add files via upload
    Usage of else if.

---
+++ b.java
@@ -0,0 +1,27 @@
+//Usage of else if.
+import java.util.*;
+public class b {
+    public static void main(String[]args){
+        int n=4;
+        if(n==1)
+        {
+        System.out.println("Monday");
+        }
+        else if(n==2){
+        System.out.println("Tuesday");
+        }
+        else if(n==3){
+        System.out.println("Wednesday");
+        }
+        else if(n==4){
+        System.out.println("Thursday");
+        }
+        else if(n==5){
+        System.out.println("Friday");
+        }
+        else{
+        System.out.println("Sunday");
+        }
+    }
+    
+}
