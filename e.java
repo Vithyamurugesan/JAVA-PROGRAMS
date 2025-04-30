49ce20e988df61717d826175c68ca711b5fcd332
Vithyamurugesan <vithyabtech54@gmail.com>
2025-04-30 11:49:36 +0530

    Add files via upload
    Simple do while loop.

---
+++ e.java
@@ -0,0 +1,14 @@
+//Simple do while loop.
+import java.util.*;
+public class e {
+    public static void main(String[]args){
+        int i=5;
+        do{
+            System.out.println("Hi "+i);
+            i++;
+        }
+        while(i<=4);
+
+    }
+    
+}
